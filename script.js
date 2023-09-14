786eb0d12cd7a7f9a205693b2397bd208624dfaa
DevGabriel533 <gabriel.queiroz1711@gmail.com>
2023-09-14 18:02:07 +0100

    Utilizado Operadores Lógicos

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 /* Desenvolva seu código abaixo */
 function playRockPaperScissor(player1, player2) {
-    if (player1 === "Pedra" && player2 === "Tesoura") {
-      return "Jogador 1 venceu!";
-    } else if (player1 === "Papel" && player2 === "Pedra") {
-      return "Jogador 1 venceu!";
-    } else if (player1 === "Tesoura" && player2 === "Papel") {
-      return "Jogador 1 venceu!";
-    } else if (player2 === "Pedra" && player1 == "Tesoura") {
-      return "Jogador 2 venceu!";
-    } else if (player2 === "Papel" && player1 == "Pedra") {
-      return "Jogador 2 venceu!";
-    } else if (player2 === "Tesoura" && player1 === "Papel") {
-      return "Jogador 2 venceu!";
-    }
-    return `Empate!`;
+
+  if (player1 == "Pedra" && player2 == "Pedra" || player1 == "Papel" && player2 == "Papel" || player1 == "Tesoura" && player2 == "Tesoura") {
+
+    return "Empate!"
+
+  } else if (player1 == "Pedra" && player2 == "Tesoura" || player1 == "Tesoura" && player2 == "Papel" || player1 == "Papel" && player == "Pedra") {
+
+    return "Jogador 1 venceu!"
+      
+  } else if (player1 == "Tesoura" && player2 == "Pedra" || player1 == "Papel" && player2 == "Tesoura" || player1 == "Pedra" && player2 == "Papel") {
+
+    return "Jogador 2 venceu!"
   }
+}
