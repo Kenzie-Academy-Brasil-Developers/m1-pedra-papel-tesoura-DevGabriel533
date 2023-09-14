29a71bd3ac87d93790853956f212bd3054b12614
DevGabriel533 <gabriel.queiroz1711@gmail.com>
2023-09-14 20:11:07 +0100

    Arrumado o parametro "player" na linha 8

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function playRockPaperScissor(player1, player2) {
 
     return "Empate!"
 
-  } else if (player1 == "Pedra" && player2 == "Tesoura" || player1 == "Tesoura" && player2 == "Papel" || player1 == "Papel" && player == "Pedra") {
+  } else if (player1 == "Pedra" && player2 == "Tesoura" || player1 == "Tesoura" && player2 == "Papel" || player1 == "Papel" && player2 == "Pedra") {
 
     return "Jogador 1 venceu!"
       
